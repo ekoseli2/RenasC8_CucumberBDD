480d87fcd702410b132fac9068288d8a5b53cd70
Ekrem Koseli <ekoseli2@illinois.edu>
2023-04-21 19:11:50 -0500

    Break 2 Lecture 5

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/java/resources",               // location of your .feature files (copy content root)
         glue = "stepDefinitions",                                  // location of your step definitions (copy source root)
-        tags = "@TC400",                                            // Which tags will this file run?
+        tags = "@TC300",                                            // Which tags will this file run?
         dryRun = false,                                                   // we will not run undefined steps
         plugin = {
                 "pretty",                                                               // This will help us to have a more understandable console output

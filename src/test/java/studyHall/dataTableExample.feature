60ae6064e298468fb29fece88cc697898cb2f24f
oralridvan <oral.ridvan@hotmail.com>
2023-04-24 18:58:55 -0400

    Study hall

--- src/test/java/studyHall/dataTableExample.feature
+++ src/test/java/studyHall/dataTableExample.feature
@@ -1,6 +1,19 @@
 Feature: Testing dashboard with Data table
 
+  @ebaytest
   Scenario: Verify dashboard on header using List Format
     Given That user wants to navigate to website as "https://www.ebay.com/"
     Then The user wants to verify all the dasboard headers as following
-    |Saved | Motors | Electronics | Collectibles | Home & Garden | Fashion | Toys | Sporting Goods | Business & Industrial| Jewelry & Watches| eBay Live | Refurbished|
+      | Saved                 |
+      | Motors                |
+      | Electronics           |
+      | Collectibles          |
+      | Home & Garden         |
+      | Fashion               |
+      | Toys                  |
+      | Sporting Goods        |
+      | Business & Industrial |
+      | Jewelry & Watches     |
+      | eBay Live             |
+      | Refurbished           |
+      | Maria                 |
--- src/test/java/studyHall/stepsDefinitions/ebaySteps.java
+++ src/test/java/studyHall/stepsDefinitions/ebaySteps.java
@@ -3,23 +3,57 @@ package studyHall.stepsDefinitions;
 import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+import studyHall.pages.ebayPage;
 import utilities.BrowserUtils;
 import utilities.Driver;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ebaySteps extends BrowserUtils {
 
+    ebayPage ebayPage= new ebayPage();
     @Given("That user wants to navigate to website as {string}")
     public void that_user_wants_to_navigate_to_website_as(String url) {
         Driver.getDriver().get(url);
         staticWait(2);
     }
-    @Then("The user wants to verify all the dasboard headers as following")
-    public void the_user_wants_to_verify_all_the_dasboard_headers_as_following(DataTable dataTable) {
-        List<String> expectedTabs=dataTable.asList(String.class);
 
+    @Then("The user wants to verify all the dasboard headers as following")
+    public void the_user_wants_to_verify_all_the_dasboard_headers_as_following(io.cucumber.datatable.DataTable dataTable) throws Exception {
+
+        System.out.println(dataTable);
+
+        List<String> expectedTabs = dataTable.asList(String.class);
+
+        System.out.println(expectedTabs);
+        boolean isValuePresent= false;
+        List<String> actualTabs=new ArrayList<>();
+
+        for (String each1: expectedTabs){
+            isValuePresent=false;
+            for (WebElement each: ebayPage.dashboardTabs){
+                actualTabs.add(each.getText());
+                if (each1.equals(each.getText())){
+                    Assert.assertTrue(each1.equals(each.getText()));
+                    isValuePresent=true;
+                    break;
+                }
+
+            }
+            if (isValuePresent=true){
+
+                System.out.println(each1+" is present in actualtabs");
+            } else if (isValuePresent=false){
+
+                throw new Exception(each1+" is not present in actual tabs");
+            }
+        }
 
 
     }
+
+
 }
